386603e41d8375bae23b0acf535aeb4ec4d75878
Josh Holbrook <josh.holbrook@gmail.com>
2024-12-31 13:21:05 -0900

    Was running make_crc twice, lol

--- tests/test_packet.py
+++ tests/test_packet.py
@@ -1,16 +1,16 @@
-from crystalfontz.packet import parse_packet, make_crc
+from crystalfontz.packet import make_crc, parse_packet
+
 
 def test_packet() -> None:
     # key action, 1, \x04, crc=\xdc\x95
-    buffer = b'\x80\x01\x04\xdc\x95'
+    buffer = b"\x80\x01\x04\xdc\x95"
     assert parse_packet(buffer) == ((0x80, 0x04), b"")
 
 
 def test_make_crc() -> None:
     # key action, 1, \x04, crc=\xdc\x95
-    buffer = b'\x80\x01\x04\xdc\x95'
+    buffer = b"\x80\x01\x04\xdc\x95"
 
-    crc = make_crc(make_crc(buffer[:-2]))
+    crc = make_crc(buffer[:-2])
 
     assert crc == b"\xdc\x95"
-
