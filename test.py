90e2a08e70a77fb29f65091c6222d0ca5afcb70e
Josh Holbrook <josh.holbrook@gmail.com>
2024-12-31 11:47:01 -0900

    Test program will test reports

--- test.py
+++ test.py
@@ -3,8 +3,12 @@ import asyncio
 from crystalfontz.client import create_connection
 
 async def main() -> None:
-    conn = await create_connection("/dev/ttyUSB0")
-    print(conn)
+    client = await create_connection("/dev/ttyUSB0")
+
+    for i in range(10):
+        print(await client.reports.get())
+
+    client.close()
 
 
 asyncio.run(main())
