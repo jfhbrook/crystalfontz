dab0dcb216742e3aee2dbddd69b625f25a3d106c
Josh Holbrook <josh.holbrook@gmail.com>
2025-01-03 21:08:45 -0900

    Gpio from_bytes

--- crystalfontz/gpio.py
+++ crystalfontz/gpio.py
@@ -1,5 +1,6 @@
 from enum import Enum
-from typing import NoReturn, Optional, Self
+from typing import NoReturn, Optional, Self, Type
+import warnings
 
 
 class GpioFunction(Enum):
@@ -70,3 +71,38 @@ class GpioSettings:
 
     def to_bytes(self: Self) -> bytes:
         return (self.function.value + self.mode).to_bytes()
+
+    @classmethod
+    def from_bytes(cls: Type[Self], data: bytes) -> Self:
+        if len(data) != 1:
+            raise ValueError("GPIO settings expected to be 8 bits")
+
+        function = GpioFunction.USED if data[0] & 0b1000 else GpioFunction.UNUSED
+        mode = data[0] & 0b0111
+        when_up: Optional[GpioDriveMode] = None
+        when_down: Optional[GpioDriveMode] = None
+        if mode == 0b000:
+            when_up = GpioDriveMode.FAST_STRONG
+            when_down = GpioDriveMode.RESISTIVE
+        elif mode == 0b001:
+            when_up = GpioDriveMode.FAST_STRONG
+            when_down = GpioDriveMode.FAST_STRONG
+        elif mode == 0b010:
+            when_up = GpioDriveMode.HI_Z
+            when_down = None
+        elif mode == 0b011:
+            when_up = GpioDriveMode.RESISTIVE
+            when_down = GpioDriveMode.FAST_STRONG
+        elif mode == 0b100:
+            when_up = GpioDriveMode.SLOW_STRONG
+            when_down = GpioDriveMode.HI_Z
+        elif mode == 0b101:
+            when_up = GpioDriveMode.SLOW_STRONG
+            when_down = GpioDriveMode.SLOW_STRONG
+        elif mode == 0b110:
+            warnings.warn(f"Drive mode {mode:0b} is reserved")
+        else:
+            when_up = GpioDriveMode.HI_Z
+            when_down = GpioDriveMode.SLOW_STRONG
+
+        return cls(function=function, mode=mode, when_up=when_up, when_down=when_down)
