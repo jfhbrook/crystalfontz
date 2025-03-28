4b09fa71a50c65b75a7fc3f7c58973d3b3ca4592
Josh Holbrook <josh.holbrook@gmail.com>
2025-03-28 09:37:02 -0800

    Make linting happy for now

--- crystalfontz/dbus/interface.py
+++ crystalfontz/dbus/interface.py
@@ -3,11 +3,11 @@ import logging
 from typing import Optional, Self
 
 from sdbus import (  # pyright: ignore [reportMissingModuleSource]
-    dbus_method_async,
+    # dbus_method_async,
     dbus_property_async,
-    dbus_signal_async,
+    # dbus_signal_async,
     DbusInterfaceCommonAsync,
-    DbusUnprivilegedFlag,
+    # DbusUnprivilegedFlag,
 )
 
 from crystalfontz.client import Client, create_connection
