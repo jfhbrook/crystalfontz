077f8e81ccbf724a5a814cd0549bf933d586780c
Josh Holbrook <josh.holbrook@gmail.com>
2025-04-03 08:42:07 -0800

    ./scripts/integration.sh takes all arguments, shows help

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -5,14 +5,38 @@ set -euo pipefail
 COMPONENTS=()
 ARGV=()
 
+HELP='USAGE: ./scripts/integration.sh [OPTIONS] [COMPONENTS]
+
+Run integration tests for the supplied components. By default, runs cli tests.
+
+Components:
+    cli   Run crystalfontz cli integration tests
+    dbus  Start cystalfontzd and run crystalfontzctl integration tests
+
+Options:
+    --help    Show this help text
+    --system  Run any dbus tests against the system bus
+
+    Other options are passed to pytest.
+
+Environment:
+    CRYSTALFONTZ_CONFIG_FILE  Use an alternative config file. The default is
+                              ./tests/fixtures/crystalfontz.yaml.
+    CRYSTALTONTZ_LOG_LEVEL    
+'
+
 while [[ $# -gt 0 ]]; do
   case "${1}" in
-    --system)
-      ARGV+=(--system)
+    --help)
+      echo "${HELP}"
+      exit 0
+      ;;
+    cli|dbus)
+      COMPONENTS+=("${1}")
       shift
       ;;
     *)
-      COMPONENTS+=("${1}")
+      ARGV+=("${1}")
       shift
       ;;
   esac
