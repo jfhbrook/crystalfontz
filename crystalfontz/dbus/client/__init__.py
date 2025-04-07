dbe58b07480a94f93396c8161e8d7b0a393caadd
Josh Holbrook <josh.holbrook@gmail.com>
2025-04-07 05:52:27 -0800

    Back out client methods

--- crystalfontz/dbus/client/__init__.py
+++ crystalfontz/dbus/client/__init__.py
@@ -5,11 +5,10 @@ from sdbus import SdBus  # pyright: ignore [reportMissingModuleSource]
 
 from crystalfontz.config import Config
 from crystalfontz.dbus.config import StagedConfig
-from crystalfontz.dbus.domain import ConfigM, RetryTimesM, TimeoutM
+from crystalfontz.dbus.domain import ConfigM
 from crystalfontz.dbus.effects import DbusEffectClient
 from crystalfontz.dbus.interface import DBUS_NAME, DbusInterface
 from crystalfontz.dbus.report import DbusClientReportHandler
-from crystalfontz.effects import Marquee, Screensaver
 
 
 class DbusClient(DbusInterface):
@@ -40,57 +39,4 @@ class DbusClient(DbusInterface):
         return StagedConfig(
             target_config=Config.from_file(active_config.file),
             active_config=active_config,
-        )
-
-    async def effect_client(self: Self) -> DbusEffectClient:
-        if not self._effect_client:
-            self._effect_client = await DbusEffectClient.load(
-                self, TimeoutM.none, RetryTimesM.none
-            )
-        return self._effect_client
-
-    async def marquee(
-        self: Self,
-        row: int,
-        text: str,
-        pause: Optional[float] = None,
-        tick: Optional[float] = None,
-        timeout: Optional[float] = None,
-        retry_times: Optional[int] = None,
-    ) -> Marquee:
-        """
-        Display a marquee effect on the LCD screen.
-        """
-
-        client = await self.effect_client()
-
-        return Marquee(
-            row,
-            text,
-            client=client,
-            pause=pause,
-            tick=tick,
-            timeout=timeout,
-            retry_times=retry_times,
-        )
-
-    async def screensaver(
-        self: Self,
-        text: str,
-        tick: Optional[float] = None,
-        timeout: Optional[float] = None,
-        retry_times: Optional[int] = None,
-    ) -> Screensaver:
-        """
-        Display a screensaver effect on the LCD screen.
-        """
-
-        client = await self.effect_client()
-
-        return Screensaver(
-            text,
-            client=client,
-            tick=tick,
-            timeout=timeout,
-            retry_times=retry_times,
         )
